f86cda9fa847cbc220312dc85c7e6a76de4d459c
java-ninano <kgstnwls@naver.com>
2020-09-29 11:40:11 +0900

    first

---
+++ src/chap02/Variable.java
@@ -0,0 +1,20 @@
+package chap02;
+
+public class Variable {
+ public static void main(String[] args) {
+	int myVar;
+	myVar = 3;
+	// maVar ="hello";
+			
+			System.out.println(myVar);
+	
+	int myVar2 =5;
+	
+	 
+	 
+	 
+	 
+	 
+	 
+}
+}
---
+++ src/chap02/variabe/Statements.java
@@ -0,0 +1,13 @@
+package chap02.variabe;
+
+public class Statements {
+	public static void main(String[] args) {
+		System.out.println("첫번째 명령문");
+		System.out.println("두번재 명령문");
+		System.out.println("세번재 명령문");
+		
+		
+		
+		
+	}
+}
